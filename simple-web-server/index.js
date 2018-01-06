dddcacd71254b6ebc9570bfd5afd0e77e5f44e56
Christopher Espiritu <christopheredrian@gmail.com>
2018-01-06 11:37:00 +0800

    Finished with simple web server

--- simple-web-server/index.js
+++ simple-web-server/index.js
@@ -3,6 +3,9 @@ const url = require("url");
 const path = require("path");
 const fs = require("fs");
 
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
+
 const mimeTypes = {
   html: "text/html",
   jpeg: "image/jpeg",
@@ -12,16 +15,20 @@ const mimeTypes = {
   css: "text/css"
 };
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
+/**
+ * Creates our server which serves static files given
+ * on our mime types
+ */
 const server = http.createServer((req, res) => {
-  // Gets the request relative uri
+  // Gets the request relative uri (from fb.com/home => /home)
   let uri = url.parse(req.url).pathname;
+  // joins the current working directory to typesthe relative path above
   let fileName = path.join(process.cwd(), unescape(uri));
   console.log(`Loading ${uri}`);
   let stats;
   try {
+    // returns file status (info about file)
+    // essentially a wrapper to the C/C++ stats
     stats = fs.lstatSync(fileName);
   } catch ($e) {
     console.log($e);
@@ -31,26 +38,30 @@ const server = http.createServer((req, res) => {
     return;
   }
   if (stats.isFile()) {
-    let mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+    // here we can see the usage of the lstat
+    let mimeType =
+      mimeTypes[
+        path
+          .extname(fileName)
+          .split(".")
+          .reverse()[0]
+      ];
     console.log(mimeType);
     res.writeHead(200, { "Content-type": mimeType });
     let fileStream = fs.createReadStream(fileName);
     fileStream.pipe(res);
   } else if (stats.isDirectory()) {
     res.writeHead(302, {
-      "Location": "index.html"
+      Location: "index.html"
     });
     res.end();
-  } else{
+  } else {
     res.writeHead(500, {
-      "Content-type": "text/plain",
-    })
-    res.write("500 Internal Error\n")
+      "Content-type": "text/plain"
+    });
+    res.write("500 Internal Error\n");
     res.end();
   }
-  // res.statusCode = 200;
-  // // res.setHeader("Content-Type", "text/plain");
-  // res.end("Hello World\n");
 });
 
 server.listen(port, hostname, () => {
